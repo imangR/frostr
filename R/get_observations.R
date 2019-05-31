753ea9faed017ab36457f53fd2a3eff8b5e05811
Iman G <ghayoornia.iman@gmail.com>
2019-05-31 02:06:00 +0200

    Update get_observations.R

--- R/get_observations.R
+++ R/get_observations.R
@@ -1,3 +1,93 @@
+#' @title Get historical weather observations from the Frost API
+#'
+#' @description \code{get_observations()} retrieves historical weather data from
+#'   the Frost API.
+#'
+#' @usage \code{get_observations(client_id, sources, reference_time, elements,
+#'   ...)}
+#'
+#' @param client_id A string. The client ID to use to send requests to the Frost
+#'   API.
+#'
+#' @param sources A character vector. The IDs of the data sources (i.e.
+#'   stations) that observed the weather data.
+#'
+#' @param reference_time A string. The time range to get observations for in
+#'   either extended ISO-8601 format or the single word "latest".
+#'
+#' @param elements A character vector. The elements to get observations for.
+#'   Available elements can be retrieved with the \code{get_elements()}
+#'   function.
+#'
+#' @param maxage A string. The maximum observation age as an ISO-8601 period,
+#'   such as "P1D". This parameter is only applicable when \code{reference_time
+#'   = "latest"}. The default value is "PT3H".
+#'
+#' @param limit A string or a positive integer. The maximum number of
+#'   observation times to be returned for each combination of source and
+#'   element, counting from the most recent time. This parameter is only
+#'   applicable when \code{reference_time = "latest"}. Set \code{limit = "all"}
+#'   to get all available times or a positive integer. The default value is 1.
+#'
+#' @param time_offsets A character vector. The time offsets to get observations
+#'   for provided as a vector of ISO-8601 periods. If the parameter is not set,
+#'   then the response (i.e. output) is not filtered on time offsets.
+#'
+#' @param time_resolutions A character vector. The time resolutions to get
+#'   observations for provided as a vector of ISO-8601 periods. If the parameter
+#'   is not set, then the response (i.e. output) is not filtered on time
+#'   resolutions.
+#'
+#' @param time_series_ids A numeric vector. The internal time series IDs to get
+#'   observations for as a vector of integers. If the parameter is not set, then
+#'   the response (i.e. output) is not filtered on internal time series ID.
+#'
+#' @param performance_categories A character vector. The performance categories
+#'   to get observations for as a vector of letters, e.g. "A", "C". If the
+#'   parameter is not set, then the response (i.e. output) is not filtered on
+#'   internal time series ID.
+#'
+#' @param exposure_categories A numeric vector. The exposure categories to get
+#'   observations for as a vector of integers. If the parameter is not set, then
+#'   the response (i.e. output) is not filtered on exposure categories.
+#'
+#' @param qualities A numeric vector. The qualities to get observations for as a
+#'   vector of integers. If the parameter is not set, then the response (i.e.
+#'   output) is not filtered on quality.
+#'
+#' @param levels A numeric vector. The sensor levels to get observations for as
+#'   a vector of integers. If the parameter is not set, then the response (i.e.
+#'   output) is not filtered on sensor level.
+#'
+#' @param include_extra A logical or integer. If this parameter is set to 1
+#' or \code{TRUE}, and extra data is available, then this data is included
+#' in the response. Extra data currently consists of the original observation
+#' value and the 16-character control info.
+#'
+#' @param fields A character vector. Fields to include in the response (i.e.
+#'   output). If this parameter is specified, then only these fields are
+#'   returned in the response. If not specified, then all fields will be
+#'   returned in the response.
+#'
+#' @param return_response A logical. If set to \code{TRUE}, then the function
+#'   returns the response from the GET request. If set to \code{FALSE}, then the
+#'   function returns a dataframe of the content in the response to the GET
+#'   request.
+#'
+#' @return The function returns either a data frame of historical weather
+#' observations or the response of the GET request for the observations,
+#' depending on the value set for the \code{return_response} argument.
+#'
+#' @examples
+#'
+#' @importFrom httr GET
+#' @importFrom httr content
+#' @importFrom httr stop_for_status
+#' @importFrom jsonlite fromJSON
+#' @importFrom tibble as_tibble
+#' @importFrom tidyr unnest
+#' @export get_observations
+
 get_observations <-
   function(
     client_id,
@@ -18,6 +108,8 @@ get_observations <-
     return_response = FALSE
   ) {
 
+    # Insert input argument control function ----
+
     input_args <-
       list(
         sources               = frost_csl(sources),
@@ -32,7 +124,7 @@ get_observations <-
         exposurecategories    = frost_csl(exposure_categories),
         qualities             = frost_csl(qualities),
         levels                = frost_csl(levels),
-        includeextra          = include_extra,
+        includeextra          = as.integer(include_extra),
         fields                = frost_csl(fields)
         )
 
@@ -42,7 +134,8 @@ get_observations <-
 
     r <- httr::GET(url, query = input_args)
 
-    stop_for_status(r)
+    # Insert a function that stops for Frost API status messages ----
+    httr::stop_for_status(r)
     stop_for_type(r)
 
     if (return_response) return(r)
