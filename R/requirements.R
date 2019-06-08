2ea272caa9a691b664473a27a7a398268cdd4235
ghim <ghayoornia[dot]iman[at]gmail[dot]com>
2019-06-08 23:36:10 +0200

    Create requirements.R

---
+++ R/requirements.R
@@ -0,0 +1,14 @@
+#' @importFrom httr GET
+#' @importFrom httr content
+#' @importFrom httr stop_for_status
+#' @importFrom httr user_agent
+#' @importFrom jsonlite fromJSON
+#' @importFrom tibble as_tibble
+#' @importFrom tidyr unnest
+#' @export get_obs
+#' @export get_obs_timeseries
+#' @export get_obs_qualitycodes
+#' @export get_sources
+#' @export get_locations
+#' @export get_elements
+#' @export get_element_codetables
